90402b2330a41043e6768db54438cb225e243bb8
Shailav <justashailav02@gmail.com>
2025-12-24 14:05:14 +0530

    Initial frontend commit

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const productSlice = createSlice({
-  name: "Product",
+  name: "products",
   initialState: {
     loading: false,
     error: null,
