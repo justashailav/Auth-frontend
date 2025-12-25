531758b805b4b357726a644097ced26c2d1eee7a
Shailav <justashailav02@gmail.com>
2025-12-25 13:51:09 +0530

    Initial frontend commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import CartSidebar from "./CartSidebar";
+import CartSidebar from "../Pages/User/CartSlidebar";
+
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
