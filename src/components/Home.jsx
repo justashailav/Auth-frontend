43b4a7304f2562a0bc8f552faabd73eaf41c6a33
Shailav <justashailav02@gmail.com>
2025-12-25 10:10:49 +0530

    Initial frontend commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     dispatch(getAllProducts())
   },[dispatch])
   return (
-     <div>
+     <div className="mt-10">
       <h1>Product List</h1>
 
       {productList.map((product) => (
