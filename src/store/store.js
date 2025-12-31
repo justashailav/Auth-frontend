656fc76eb892ea2e1281214f8b016869009063cf
Shailav <justashailav02@gmail.com>
2025-12-31 14:00:07 +0530

    Initial frontend commit

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,13 @@ import authReducer from "./slices/authSlice"
 import productReducer from "./slices/productSlice"
 import cartReducer from "./slices/cartSlice"
 import addressReducer from "./slices/addressSlice"
+import orderReducer from "./slices/orderSlice"
 export const store=configureStore({
     reducer:{
         auth:authReducer,
         products:productReducer,
         cart:cartReducer,
-        address:addressReducer
+        address:addressReducer,
+        order:orderReducer
     }
 }) 
